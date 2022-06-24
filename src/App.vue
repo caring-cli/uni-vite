a8575b87130493a5ff6608f75506fa65b455d642
风度万人迷 <805391114@qq.com>
2022-06-24 10:12:20 +0800

    version:0.0.1

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  onLaunch () {
+    console.log('App Launch')
+  },
+  onShow () {
+    console.log('App Show')
+  },
+  onHide () {
+    console.log('App Hide')
+  },
+}
+</script>
+
+<style lang="scss">
+page {
+  background: #f8f8f8;
+}
+</style>
